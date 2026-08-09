6ca65df41710493cfc0d1940427ee5b0d761bcc5
AsheleyAudrey <ashieley7@gmail.com>
2026-08-09 22:35:34 +0000

    updated cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import boto3
 from schemes import EventCreate, EventRegistration
 from uuid import uuid4
@@ -11,6 +12,15 @@ load_dotenv()
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
